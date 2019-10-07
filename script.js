2692718006317216b86cf741586bd3c32b7657a4
braydend7666 <43589692+braydend7666@users.noreply.github.com>
2019-10-07 10:22:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Brayden DAngelo"
+let userName = "braydend7666";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
